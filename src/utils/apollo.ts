1d4ad0ec7f42c0fe2ef8ad6cdd8019355c40485b
Eduardo Pacheco Celeste <eduardo.celeste@ztech.net>
2022-03-31 03:10:08 -0300

    refactor: move apollo to other structure

---
+++ src/utils/apollo.ts
@@ -0,0 +1,37 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
+import { useMemo } from "react";
+
+let apolloClient: ApolloClient<NormalizedCacheObject>;
+
+function createApolloClient() {
+  return new ApolloClient({
+    uri: process.env.REACT_APP_PIPEFY_API_URL || "",
+    cache: new InMemoryCache(),
+    headers: {
+      authorization: `Bearer ${process.env.REACT_APP_PIPEFY_API_TOKEN || ""}`,
+    },
+    credentials: "same-origin",
+  });
+}
+
+export function initializeApollo(initialState = {}) {
+  const apolloClientGlobal = apolloClient ?? createApolloClient();
+
+  // recuperando os dados do cache
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState);
+  }
+
+  apolloClient = apolloClient ?? apolloClientGlobal;
+
+  return apolloClient;
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
+}
